8b09a3e62c3a11a4f71ab30da577af6c2aede54f
Yashwanth Manchikatla <yashwanth.manchikatla@gmail.com>
2017-02-02 12:55:00 -0600

    Update OrderedSet.java

--- src/com/yashu/interview/map/linkedhashset/OrderedSet.java
+++ src/com/yashu/interview/map/linkedhashset/OrderedSet.java
@@ -1,4 +1,6 @@
-package HashSet;
+package com.yashu.interview.map.linkedhashset;
+
+import java.util.Iterator;
 
 /**
   Collection used to store list of elements and retains insertion order.
@@ -17,18 +19,18 @@ public interface OrderedSet<T> {
 	@throws InputMismatchException if the element doesn't exist.
 	 
 	 **/
-	public void delete(T value);
+	public boolean delete(T value);
 	
 	/**
 		Checks uniqueness of the list during insertion.
 	 **/
 	public boolean exists(T value);
 	
-
 	/**
-	 * Prints all of the elements in the set.
+	 * Iterates through all the elements in the list.
+	 * @return Iterator Object.
 	 */
-	public void print();
+	public Iterator<Object> iterator();
 	
 	
 }
