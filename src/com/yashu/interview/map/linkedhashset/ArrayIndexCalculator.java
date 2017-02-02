bb048bde1517036f595e53a4499b6da2d4193f31
Yashwanth Manchikatla <yashwanth.manchikatla@gmail.com>
2017-02-02 12:39:43 -0600

    Update ArrayIndexCalculator.java
    Throws exception when null object or 0/-ve array length are passed.

--- src/com/yashu/interview/map/linkedhashset/ArrayIndexCalculator.java
+++ src/com/yashu/interview/map/linkedhashset/ArrayIndexCalculator.java
@@ -1,9 +1,13 @@
-package HashSet;
+package com.yashu.interview.map.linkedhashset;
 
 public class ArrayIndexCalculator {
 	public int getIndex(Object obj, int arrayLength) {
+		if(arrayLength<=0 || obj == null){
+			throw new IllegalArgumentException();
+		}else{
 		int hash = obj.hashCode();
 		return Math.abs(hash%arrayLength);  
+		}
 	}
 
 }
