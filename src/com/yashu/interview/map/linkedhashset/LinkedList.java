8c3c470e43af248445bdd4df1b9738b8b33136cd
Yashwanth Manchikatla <yashwanth.manchikatla@gmail.com>
2017-02-02 12:54:04 -0600

    Update LinkedList.java

--- src/com/yashu/interview/map/linkedhashset/LinkedList.java
+++ src/com/yashu/interview/map/linkedhashset/LinkedList.java
@@ -1,19 +1,17 @@
-package HashSet;
+package com.yashu.interview.map.linkedhashset;
 
- class LinkedList<T> {
+
+class LinkedList<T> {
 	private Node<T> head;
 	private Node<T> tail;
-
-	public Node<T> getHead() {
+	
+	public Node<T> getHead(){
 		return head;
-	}
-	public void setHead(Node<T> head) {
-		this.head = head;
 	}
 	public Node<T> getTail() {
 		return tail;
 	}
-	public void setTail(Node<T> tail) {
+	void setTail(Node<T> tail) {
 		this.tail = tail;
 	}
 
@@ -48,5 +46,6 @@ package HashSet;
 		}
 		
 	}
+	 
 
 }
